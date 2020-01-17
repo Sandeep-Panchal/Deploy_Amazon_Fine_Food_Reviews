830a97dc2b52684099200eadaf7ce6538768196b
Sandeep-Panchal <42773950+Sandeep-Panchal@users.noreply.github.com>
2020-01-17 20:33:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, url_for
+import pickle
+
+# Loading files
+clf = pickle.load(open('twt_nb_model.pkl', 'rb'))
+cv = pickle.load(open('twt_transform.pkl', 'rb'))
+
+# Initiate flask
+app = Flask(__name__)
+
+# Route for home page
+@app.route('/')
+def home():
+    return render_template('Tweets_Predict_Page.html')
+    
+
+# Route for predictor page
+@app.route('/Predict', methods = ['POST'])
+def Predict():
+    
+    if request.method == 'POST':
+        tweet = request.form['message']
+        data = [tweet]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('Tweets_Predict_Page.html', prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = False)
